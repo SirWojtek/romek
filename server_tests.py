285061a37e460179f9d01844bcff851059817ae5
Okulewicz Mateusz <mateusz.okulewicz@nokia.com>
2015-11-17 12:21:30 +0100

    Added tests for server

--- server_tests.py
+++ server_tests.py
@@ -9,12 +9,12 @@ server = None
 def init_object(bus):
     iterations = 10
     sleep_time = 0.01
-
     for i in range(iterations):
         try:
             return bus.get_object('org.romek.service', '/org/romek/service')
         except dbus.DBusException:
-            sleep(sleep_time)   
+            sleep(sleep_time)
+    raise Exception('Could not get server object')
 
 class TestServerSchedule(unittest.TestCase):
     def setUp(self):
@@ -24,21 +24,30 @@ class TestServerSchedule(unittest.TestCase):
         self.interface = 'org.romek.interface'
         self.obj = init_object(self.bus)
 
-        self.task1 = ('MO', (12,0), 'MO', (13,0), 13)
-        self.task2 = ('F', (14,30), 'SA', (6,15), 21)
+        self.task1 = ('MO', (12, 0), 'MO', (13, 0), 13)
+        self.task2 = ('F', (14, 30), 'SA', (6, 15), 21)
+        self.task3 = ('SU', (9, 30), 'TU', (9, 30), 28)
 
     def tearDown(self):
         global server
         server.kill()
 
-    def test_add_single_task(self):
-        self.assertTrue(self.obj.add_schedule_task(self.task1, dbus_interface = self.interface))
-        self.assertListEqual(self.obj.list_schedule_task(), [ self.task1 ])
-
     def test_add_multiple_tasks(self):
         self.assertTrue(self.obj.add_schedule_task(self.task1, dbus_interface = self.interface))
         self.assertTrue(self.obj.add_schedule_task(self.task2, dbus_interface = self.interface))
         self.assertListEqual(self.obj.list_schedule_task(), [ self.task1, self.task2 ])
+
+    def test_add_multiple_tasks_and_remove_task(self):
+        self.assertTrue(self.obj.add_schedule_task(self.task1, dbus_interface = self.interface))
+        self.assertTrue(self.obj.add_schedule_task(self.task2, dbus_interface = self.interface))
+        self.assertTrue(self.obj.remove_schedule_task(self.task1, dbus_interface = self.interface))
+        self.assertListEqual(self.obj.list_schedule_task(), [ self.task2 ])
+
+    def test_add_multiple_tasks_and_update_task(self):
+        self.assertTrue(self.obj.add_schedule_task(self.task1, dbus_interface = self.interface))
+        self.assertTrue(self.obj.add_schedule_task(self.task2, dbus_interface = self.interface))
+        self.assertTrue(self.obj.update_schedule_task((self.task1, self.task3), dbus_interface = self.interface))
+        self.assertListEqual(self.obj.list_schedule_task(), [ self.task3, self.task2 ])
 
 if __name__ == '__main__':
     unittest.main()

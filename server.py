26712b39cf1c93b7a1295f6a2beda91cd087d3c5
SirWojtek <momatoku@gmail.com>
2015-11-19 11:28:44 +0100

    Added manual mode implementation

--- server.py
+++ server.py
@@ -38,27 +38,32 @@ class RomekServer(dbus.service.Object):
         return safe_schedule_call(partial(self._scheduler.add_task, task))
 
     @dbus.service.method(dbus_interface = interface_name,
-        in_signature = edit_task_signature, out_signature = 'b')    
+        in_signature = edit_task_signature, out_signature = 'b')
     def update_schedule_task(self, task):
         return safe_schedule_call(partial(self._scheduler.update_task, task))
 
     @dbus.service.method(dbus_interface = interface_name,
-        in_signature = task_tuple_signature, out_signature = 'b')    
+        in_signature = task_tuple_signature, out_signature = 'b')
     def remove_schedule_task(self, task):
         return safe_schedule_call(partial(self._scheduler.remove_task, task))
 
     @dbus.service.method(dbus_interface = interface_name,
-        in_signature = '', out_signature = task_list_signature)    
+        in_signature = '', out_signature = task_list_signature)
     def list_schedule_task(self):
         return self._scheduler.list_tasks()
 
     @dbus.service.method(dbus_interface = interface_name,
         in_signature = 'u', out_signature = 'b')
     def set_temperature(self, temperature):
-        self._current_settings.update_temperature(temperature)
+        self._current_settings.update_temperature_manual(temperature)
         return True
 
     @dbus.service.method(dbus_interface = interface_name,
         in_signature = '', out_signature = 'u')
     def get_temperature(self):
         return self._current_settings.temperature
+
+    @dbus.service.method(dbus_interface = interface_name,
+        in_signature = '', out_signature = 'b')
+    def get_manual_mode(self):
+        return self._current_settings.manual_mode
--- settings/current_settings.py
+++ settings/current_settings.py
@@ -6,10 +6,14 @@ class CurrentSettings(observable.Observable):
         self.manual_mode = manual_mode
         observable.Observable.__init__(self)
 
+    def update_temperature_manual(self, temperature):
+        self.update_manual_mode(True)
+        self.update_temperature(temperature)
+
     @observable.update_decorator
     def update_temperature(self, temperature):
         self.temperature = temperature
 
     @observable.update_decorator
     def update_manual_mode(self, manual_mode):
-        self._manual_mode = manual_mode
+        self.manual_mode = manual_mode

a3c8a3ebfed409b2c0278efcb86e86073215b58f
Mateusz Okulewicz <mateusz.okulewicz@gigaset.com>
2015-12-04 14:28:30 +0100

    Fixed exception caused by default, negative status value

--- settings/current_status.py
+++ settings/current_status.py
@@ -1,7 +1,7 @@
 import observable
 
 class CurrentStatus(observable.Observable):
-    def __init__(self, temperature = -1):
+    def __init__(self, temperature = 10):
         self.temperature = temperature
         observable.Observable.__init__(self)
 

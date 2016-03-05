de8d14bb61b5988e2baa8f440d59c1e75a171680
SirWojtek <momatoku@gmail.com>
2016-03-05 14:18:12 +0100

    SerialWorker handles one message an once

--- serial_port/serial_port_worker.py
+++ serial_port/serial_port_worker.py
@@ -7,31 +7,32 @@ class SerialPortWorker(threading.Thread):
     def __init__(self, receive_callback):
         threading.Thread.__init__(self)
         self._serial = serial.Serial(0, timeout = self._timeout)
-        self._messages_to_send = []
+        self._message_to_send = None
         self._send_event = threading.Event()
         self._send_lock = threading.Lock()
         self._receive_callback = receive_callback
 
     def add_message(self, message):
         self._send_lock.acquire()
-        self._messages_to_send.append(message)
+        if self._message_to_send:
+            raise Exception('Previous message not handled')
+        self._message_to_send = message
         self._send_lock.release()
         self._send_event.set()
 
     def run(self):
         while True:
             if self._send_event.wait(self._timeout):
-                self._send_lock.acquire()
                 self._send_message()
-                self._send_event.clear()
-                self._send_lock.release()
             self._receive_message()
 
     # thread safe must be ensured by caller
     def _send_message(self):
-        for message in self._messages_to_send:
-            self._serial.write(message.get())
-            self._serial.flush()
+        self._send_lock.acquire()
+        self._serial.write(self._message_to_send.get())
+        self._message_to_send = None
+        self._send_event.clear()
+        self._send_lock.release()
 
     def _receive_message(self):
         message = self._serial.readline()
